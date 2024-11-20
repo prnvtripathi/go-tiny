f0ba8df094f8d78d934986a3c2c7dcec38d01cd2
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2024-11-20 15:31:18 +0530

    Add authentication middleware to protect routes; redirect unauthenticated users to login page

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse, NextRequest } from "next/server";
+import { auth } from "./auth";
+
+export async function middleware(req: NextRequest) {
+  // check if the user is authenticated
+  const session = await auth();
+  const url = req.nextUrl.clone();
+  url.pathname = "/login";
+
+  if (!session) {
+    return NextResponse.redirect(url.toString());
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/short/:path*", "/qr/:path*", "/api/:path*"],
+};
