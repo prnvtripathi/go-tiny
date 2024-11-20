f4709b9d9bdfbf38bbe9ea835e96aef84ff4a1bb
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2024-11-20 15:34:57 +0530

    Update navigation items; add "Generate QR" option and reorder existing items

--- frontend/src/data/navItems.ts
+++ frontend/src/data/navItems.ts
@@ -1,20 +1,26 @@
 export const navItems = [
-    {
-        id: 1,
-        name: "Home",
-        link: "/",
-        tooltiptext: "Home",
-    },
-    {
-        id: 2,
-        name: "Shorten URL",
-        link: "/short",
-        tooltiptext: "Shorten URL",
-    },
-    {
-        id: 3,
-        name: "Dashboard",
-        link: "/dashboard/home",
-        tooltiptext: "Dashboard",
-    }
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+    tooltiptext: "Home",
+  },
+  {
+    id: 2,
+    name: "Shorten URL",
+    link: "/short",
+    tooltiptext: "Shorten URL",
+  },
+  {
+    id: 3,
+    name: "Generate QR",
+    link: "/qr",
+    tooltiptext: "Generate QR",
+  },
+  {
+    id: 4,
+    name: "Dashboard",
+    link: "/dashboard/home",
+    tooltiptext: "Dashboard",
+  },
 ];
